0b70a870b42dbec7c9d8a8d8207064a4386542d2
Filipe Mendonca <filipetrm@gmail.com>
2018-02-26 18:20:48 -0300

    visualizacao de redacao completa

--- client/src/main/js/controller/RedacaoController.js
+++ client/src/main/js/controller/RedacaoController.js
@@ -8,6 +8,22 @@ angular.module('sos-redacao').controller('RedacaoController', ['redacao', '$scop
 
     self.$onInit = function () {
         _.assign(self.redacao, redacao);
+
+        self.redacao.nota = soma();
+
+        console.log(self.redacao.nota);
+    };
+
+    self.voltar = function() {
+      $state.go('redator');
+    };
+
+    function soma() {
+      return self.redacao.rateI +
+          self.redacao.rateII+
+          self.redacao.rateIII +
+          self.redacao.rateIV +
+          self.redacao.rateV;
     };
 
     }]);
