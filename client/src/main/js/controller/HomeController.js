b98b220bb8e88e593aa8a7d925730719e312d215
Filipe Mendonca <filipetrm@gmail.com>
2018-03-02 15:02:56 -0300

    testando o angular

--- client/src/main/js/controller/HomeController.js
+++ client/src/main/js/controller/HomeController.js
@@ -1,11 +1,15 @@
 angular.module('sos-redacao').controller('HomeController', ['$state', function ($state) {
     var self = this;
 
+    console.log("chegou aqui");
+
     self.entrarAluno = function() {
+        console.log("rota redator");
         $state.go('redator');
     };
 
     self.entrarCorretor = function() {
+        console.log("rota corretor");
         $state.go('corretor');
     }
 
