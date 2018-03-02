feacb29072daf99df53c743fbdc35cf3e18bd46f
Filipe Mendonca <filipetrm@gmail.com>
2018-03-02 15:17:30 -0300

    banco de dados

--- server/src/main/config/dbConfig.js
+++ server/src/main/config/dbConfig.js
@@ -5,13 +5,13 @@ let db = mongoose.connection;
 
 module.exports = () => {
 
-        const profile = 'SOS-REDACAO';
+        const profile = 'sos-redacao';
 
     /**
      * Caso não haja uma variável de ambiente informando o endereço do banco de dados,
      * utilizar endereço local.
      */
-    const db_string = process.env.MONGODB_ADDRESS || 'mongodb://127.0.0.1:27017/' + profile;
+    const db_string = process.env.MONGODB_ADDRESS || 'mongodb://empsoft:sosredacao@ds255258.mlab.com:55258/sos-redacao';
     if (!db.readyState) {
         const conn = mongoose.connect(db_string);
     }
